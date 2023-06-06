d2d72b756e3cc6d370f8256ef49d1a5576883387
Hyun Min Cho <solon7833@gmail.com>
2023-06-05 22:54:35 -0300

    initial project

---
+++ src/main/java/com/bd2/chesscomp/ChesscompApplication.java
@@ -0,0 +1,13 @@
+package com.bd2.chesscomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChesscompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChesscompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd2/chesscomp/ChesscompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd2.chesscomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChesscompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
