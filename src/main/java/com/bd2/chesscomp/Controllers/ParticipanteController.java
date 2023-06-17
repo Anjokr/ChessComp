81700f85db2b24fce79a5cf0840d132efe756ebe
gustavos1lva <gustavorns42@gmail.com>
2023-06-17 19:12:15 -0300

    arrumando build

--- src/main/java/com/bd2/chesscomp/Controllers/ParticipanteController.java
+++ src/main/java/com/bd2/chesscomp/Controllers/ParticipanteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bd2.chesscomp.Models.Participante;
 import com.bd2.chesscomp.Models.Hotel;
 import com.bd2.chesscomp.Models.Jogador;
+import com.bd2.chesscomp.Models.Jogo;
 import com.bd2.chesscomp.Models.Schedule;
 import com.bd2.chesscomp.Repository.ParticipanteRepository;
 
@@ -21,10 +22,12 @@ public class ParticipanteController {
     @PostMapping("/getSchedule")
     public ResponseEntity<Schedule> getSchedule() {
         // @TODO: Pegar todos a programação de jogos e retornar para o front
+        return new ResponseEntity<Schedule>(null);
     }
 
     @PostMapping("/getGames")
     public ResponseEntity<Jogo> getGames(@RequestBody Hotel hotel, Jogador jogador, Participante participante) {
+        return new ResponseEntity<Jogo>(null);
     }
 
 }
